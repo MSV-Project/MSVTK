68cbf6b7a5ddcbf68601c971711f8bffa90a41bb
Ricardo Ortiz <ricardo.ortiz@kitware.com>
2012-10-29 15:24:45 -0400

    Update dashboard script template.
    Updated the template so it download the correct driver from midas:
    http://midas3.kitware.com/midas/download?folders=7895
    
    Look in here for additional templates.

--- CMake/TEMPLATE.msvtkDashboardScript.cmake
+++ CMake/TEMPLATE.msvtkDashboardScript.cmake
@@ -1,11 +1,11 @@
 #
-# OS: Ubuntu 11.04 3.0.0-14-generic
+# OS: Linux x86_64
 # Hardware: x86_64 GNU/Linux
 # GPU: NA
 #
 
-# Note: The specific version and processor type of this machine should be reported in the
-# header above. Indeed, this file will be send to the dashboard as a NOTE file.
+# Note: The specific version and processor type of this machine should be reported in the 
+# header above. Indeed, this file will be send to the dashboard as a NOTE file. 
 
 cmake_minimum_required(VERSION 2.8)
 
@@ -16,26 +16,32 @@ cmake_minimum_required(VERSION 2.8)
 #
 # Dashboard properties
 #
-set(MY_OPERATING_SYSTEM "Linux") # Windows, Linux, Darwin...
-set(MY_COMPILER "gcc4.6.1")
+set(MY_OPERATING_SYSTEM "Linux") # Windows, Linux, Darwin... 
+set(MY_COMPILER "GCC-4.7")
 
 if (NOT MY_QT_VERSION)
-  set(MY_QT_VERSION "474")
+  set(MY_QT_VERSION "4.7")
 endif()
 
-set(QT_QMAKE_EXECUTABLE "/home/michael/Projects/QtSDK1_1_3/Desktop/Qt/${MY_QT_VERSION}/gcc/bin/qmake.exe")
-set(CTEST_GIT_COMMAND "/usr/lib/git-core/git")
-set(CTEST_SITE "Vahalla.kitware") # for example: mymachine.kitware, mymachine.dkfz, ...
-set(CTEST_DASHBOARD_ROOT "/home/michael/Projects/DashboardScripts/MSVTK/")
+set(QT_QMAKE_EXECUTABLE "/usr/bin/qmake")
+set(CTEST_SITE "melcor.kitware") # for example: mymachine.kitware, mymachine.dkfz, ...
+set(CTEST_DASHBOARD_ROOT "$ENV{HOME}/Dashboards/MSVTK")
+
+if(NOT EXISTS "${CTEST_DASHBOARD_ROOT}/Logs")
+  file(MAKE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/Logs/")
+endif()  
 
 if (NOT MY_CMAKE_VERSION)
-   set(MY_CMAKE_VERSION "CMake 2.8.5")
+   set(MY_CMAKE_VERSION "CMake 2.8.8")
 endif()
 
-set(CTEST_CMAKE_COMMAND "/usr/bin/cmake")
-set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
-set(MY_BITNESS "64")
-
+set(CTEST_CMAKE_COMMAND   "/usr/bin/cmake")
+if (NOT CTEST_CMAKE_GENERATOR)
+  set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+endif()
+if (NOT MY_BITNESS)
+  set(MY_BITNESS "64")
+endif()
 #
 # Dashboard options
 #
@@ -45,21 +51,21 @@ set(WITH_COVERAGE FALSE)
 set(WITH_DOCUMENTATION FALSE)
 
 if (NOT CTEST_BUILD_CONFIGURATION)
-set(CTEST_BUILD_CONFIGURATION "Debug")
+  set(CTEST_BUILD_CONFIGURATION "Debug")
 endif()
 
 set(CTEST_TEST_TIMEOUT 500)
-set(CTEST_BUILD_FLAGS "-j5") # Use multiple CPU cores to build
-set(CTEST_LOG_FILE "/home/michael/Projects/DashboardScripts/MSVTK/Logs/Nightly-${CTEST_BUILD_CONFIGURATION}-${MY_BITNESS}.log")
+set(CTEST_BUILD_FLAGS "") # Use multiple CPU cores to build
+set(CTEST_LOG_FILE "${CTEST_DASHBOARD_ROOT}/Logs/Nightly_${CTEST_BUILD_CONFIGURATION}}_${MY_BITNESS}bits.log")
 
-# experimental:
+# experimental: 
 #     - run_ctest() macro will be called *ONE* time
 #     - binary directory will *NOT* be cleaned
-# continuous:
-#     - run_ctest() macro will be called EVERY 5 minutes ...
+# continuous: 
+#     - run_ctest() macro will be called EVERY 5 minutes ... 
 #     - binary directory will *NOT* be cleaned
 #     - configure/build will be executed *ONLY* if the repository has been updated
-# nightly:
+# nightly: 
 #     - run_ctest() macro will be called *ONE* time
 #     - binary directory *WILL BE* cleaned
 if (NOT SCRIPT_MODE)
@@ -69,14 +75,11 @@ endif()
 #
 # Project specific properties
 #
-set(CTEST_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}MSVTK")
-set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}MSVTK-Superbuild-${CTEST_BUILD_CONFIGURATION}-${SCRIPT_MODE}-${MY_QT_VERSION}-${MY_CMAKE_VERSION}")
+set(CTEST_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/source")
+set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/build_${CTEST_BUILD_CONFIGURATION}_${MY_BITNESS}bits_${SCRIPT_MODE}_${MY_QT_VERSION}_${MY_CMAKE_VERSION}")
 
-# Additionnal CMakeCache options - For example:
-
-set(ADDITIONNAL_CMAKECACHE_OPTION "
-  GIT_EXECUTABLE:FILEPATH=/usr/lib/git-core/git
-")
+# set additional CMakeCache options 
+set(ADDITIONAL_CMAKECACHE_OPTION "")
 
 # set any extra environment variables here
 set(ENV{DISPLAY} ":0")
@@ -84,6 +87,7 @@ set(ENV{DISPLAY} ":0")
 find_program(CTEST_COVERAGE_COMMAND NAMES gcov)
 find_program(CTEST_MEMORYCHECK_COMMAND NAMES valgrind)
 find_program(CTEST_GIT_COMMAND NAMES git)
+message ("********************************************************COMMAND : ${CTEST_GIT_COMMAND}")
 
 #
 # Git repository - Overwrite the default value provided by the driver script
@@ -93,14 +97,13 @@ find_program(CTEST_GIT_COMMAND NAMES git)
 ##########################################
 # WARNING: DO NOT EDIT BEYOND THIS POINT #
 ##########################################
-
 set(CTEST_NOTES_FILES "${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}")
 
 #
 # Project specific properties
 #
 set(CTEST_PROJECT_NAME "MSVTK")
-set(CTEST_BUILD_NAME "${MY_OPERATING_SYSTEM}-${MY_COMPILER}-QT${MY_QT_VERSION}-${MY_CMAKE_VERSION}-${CTEST_BUILD_CONFIGURATION}")
+set(CTEST_BUILD_NAME "${MY_OPERATING_SYSTEM}_${MY_COMPILER}_${MY_BITNESS}_QT${MY_QT_VERSION}_${MY_CMAKE_VERSION}_${CTEST_BUILD_CONFIGURATION}")
 
 #
 # Display build info
@@ -125,7 +128,8 @@ endmacro()
 #
 # Download and include dashboard driver script
 #
-set(url http://midas.kitware.com/bitstream/view/17238)
+set(url http://midas3.kitware.com/midas/download?items=25472)
 set(dest ${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}.driver)
 downloadFile(${url} ${dest})
+
 include(${dest})
