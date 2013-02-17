ed504ba39a9980f41cf9100967dbd9bbb2bc0220
Julien Finet <julien.finet@kitware.com>
2013-02-17 09:34:48 -0500

    Fix unistd compilation error with VisualStudio

--- Applications/ButtonClusters/msvQButtonClustersMainWindow.cxx
+++ Applications/ButtonClusters/msvQButtonClustersMainWindow.cxx
@@ -8,7 +8,7 @@
   you may not use this file except in compliance with the License.
   You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0.txt
+      http://www.apache.org/licenses/LICENSE-2.0.txt
 
   Unless required by applicable law or agreed to in writing, software
   distributed under the License is distributed on an "AS IS" BASIS,
@@ -56,7 +56,10 @@
 #include "vtkVolumeProperty.h"
 #include "vtkVolume.h"
 
-
+// STD includes
+#ifndef _WIN32
+#include <unistd.h>
+#endif
 
 // ------------------------------------------------------------------------------
 class msvQButtonClustersMainWindowPrivate
